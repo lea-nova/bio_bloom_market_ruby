8e9cae97603ea0844bcd60f7d738062cf921b623
lea-nova <lea.rulliere@outlook.fr>
2025-03-17 14:16:04 +0100

    Rectification du nom des colonnes pour éviter le camelCase dans les schéma de la base de données

--- db/migrate/20250317130032_add_cols_to_users.rb
+++ db/migrate/20250317130032_add_cols_to_users.rb
@@ -1,7 +1,7 @@
 class AddColsToUsers < ActiveRecord::Migration[8.0]
   def change
     add_column :users, :name, :string
-    add_column :users, :lastName, :string
-    add_column :users, :dateOfBirth, :string
+    add_column :users, :last_name, :string
+    add_column :users, :date_of_birth, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_17_130032) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_17_131445) do
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -25,9 +25,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_17_130032) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name", null:false
-    t.string "lastName", null:false
-    t.string "dateOfBirth", null:false
+    t.string "name"
+    t.string "last_name"
+    t.string "date_of_birth"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
