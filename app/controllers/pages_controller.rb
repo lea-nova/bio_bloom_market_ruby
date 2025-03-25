b2ee07c2072bbbc72362fd25a54fff962960ee4e
LeaR 🐈 <157827975+lea-nova@users.noreply.github.com>
2025-03-25 07:34:48 +0100

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: [:home]
-  allow_unauthenticated_access  only: [:home]
+  allow_unauthenticated_access  only: [ :home ]
   def home
+    
   end
 end
