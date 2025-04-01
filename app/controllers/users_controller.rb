3e1162d912380774e0f5cdb548a0a9d10a31f316
lea-nova <lea.rulliere@outlook.fr>
2025-04-01 13:24:14 +0200

    Rectif CI encore lint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,8 @@ class UsersController < ApplicationController
 
   def edit
   @user = User.find(params[:id])
-  end 
+  end
+  
   def update
   @user = User.find(params[:id])
   if @user.update(user_params)
